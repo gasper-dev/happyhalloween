79f0542b1bdb35a5bb1e2c3f2710bc06381d1f39
gasper-dev <gasper1one@gmail.com>
2024-10-17 21:39:07 -0600

    hw1

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+// @ts-check
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
+
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind()],
+  output: "server",
+  adapter: vercel(),
+});
