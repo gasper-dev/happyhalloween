ad5ffc811a8e39f2f45a93e9605dd9e950b77e8a
gasper-dev <gasper1one@gmail.com>
2024-10-19 09:20:08 -0600

    hw6

---
+++ src/util/fetchImageTransform.ts
@@ -0,0 +1,28 @@
+import { getCldImageUrl } from "astro-cloudinary/helpers";
+
+export let loading: boolean;
+
+export async function fetchImageTransform(id: any) {
+  loading = true;
+
+  try {
+    const getUrl = await getCldImageUrl({
+      src: id,
+      removeBackground: true,
+      flags: "relative",
+      width: "500",
+      height: "500",
+      crop: "fill",
+      gravity: "auto",
+      aspectRatio: 0.5,
+    });
+    const response = await fetch(getUrl);
+    loading = false;
+    if (!response.ok) {
+      return { status: response.status };
+    }
+    return { getUrl };
+  } catch {
+    return { status: 404 };
+  }
+}
